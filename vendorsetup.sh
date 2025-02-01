94c4570dc6a3362e686710d1ada70730f46b50da
RVSmooth <riveeks.smooth@gmail.com>
2025-02-01 11:52:49 +0530

    Fix: dir

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,16 @@
-
-VENDOR_EXTRA_PATH=$(gettop)/vendor/extra 
+VENDOR_EXTRA_PATH=$(gettop)/vendor/extra
 VENDOR_PATCHES_PATH="${VENDOR_EXTRA_PATH}/patches"
 
 
 for project_name in $(cd "${VENDOR_PATCHES_PATH}" || exit; echo */); do
-	project_path="$(tr _ / <<<"$project_name")"
+        project_path="$(tr _ / <<<"$project_name")"
 
-	cd $(gettop) || exit
-	cd "${project_path}" || exit
-	echo "Applying patches for project: ${project_name} on ${HEAD_COMMIT}"
-	if ! git am "${PATCHES_PATH}"/"${project_name}"/*.patch --no-gpg-sign; then
-		echo "Failed to apply patches for project: ${project_name}. Aborting."
-		git am --abort &> /dev/null
-	fi
+        cd $(gettop)/"${project_path}" || exit
+        echo "Applying patches for project: ${project_name} on ${HEAD_COMMIT}"
+        if ! git am "${VENDOR_PATCHES_PATH}"/"${project_name}"/*.patch --no-gpg-sign; then
+                echo "Failed to apply patches for project: ${project_name}. Aborting."
+                git am --abort &> /dev/null
+        fi
 
-	cd $(gettop) || exit
+        cd $(gettop) || exit
 done
-
-
